61e17145f5629ee86256b80bdad1ef0eb155ee6a
Joao Iguana <jiboiadamor@gmail.com>
2024-03-13 11:02:43 +0000

    experimented with multiple conditions using logical operators

--- control_flow.py
+++ control_flow.py
@@ -6,3 +6,12 @@ elif number == 0:
     print('The number is zero')
 else:
     print('The number is positive')
+
+age = 31
+
+if age >= 18 and age < 65:
+    print("Let's get it on baby!")
+elif age < 18:
+    print("We talk when you grow up...")
+else:
+    print("You're too old for me.")
